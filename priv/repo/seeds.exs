8f3a6f2cf34543bc02899ce81c0c752f39ba599b
Roman Khrebtov <r.hrebtov@busfor.com>
2017-09-03 16:15:10 +0300

    Create seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,41 @@
 # Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Cookery.Repo.insert!(%Cookery.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+# mix run priv/repo/seeds.exs
+
+alias Cookery.Data.Accounts
+alias Cookery.Data.Recipes
+
+{:ok, admin} = Accounts.create_user(%{name: "Admin", login: "admin", password: "123456"})
+{:ok, vasia} = Accounts.create_user(%{name: "Вася Пупкин", login: "vasia", password: "123456"})
+{:ok, ivan} = Accounts.create_user(%{name: "Иван Петров", login: "ivan", password: "123456"})
+
+Accounts.update_user(vasia, %{
+  avatar: %Plug.Upload{
+    content_type: "image/jpeg",
+    filename: "vasia.jpg",
+    path: Path.join([Application.app_dir(:cookery, "priv"), "repo", "seeds", "avatars", "vasia.jpg"])
+  }
+})
+Accounts.update_user(ivan, %{
+  avatar: %Plug.Upload{
+    content_type: "image/jpeg",
+    filename: "ivan.jpg",
+    path: Path.join([Application.app_dir(:cookery, "priv"), "repo", "seeds", "avatars", "ivan.jpg"])
+  }
+})
+
+Recipes.create_recipe(vasia, %{
+  "title" => "Бутерброды с семгой",
+  "description" => "Пальчики оближешь"
+})
+Recipes.create_recipe(vasia, %{
+  "title" => "Баклажаны по-китайски",
+  "description" => "Запеченые баклажаны под острым соусом"
+})
+Recipes.create_recipe(vasia, %{
+  "title" => "Оджахури по-грузински",
+  "description" => "Мясо, овощи и арматные травы в мультварке"
+})
+Recipes.create_recipe(ivan, %{
+  "title" => "Яичница с помидором",
+  "description" => "Когда в холодильнике пусто"
+})
