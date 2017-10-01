6a28440689896eb82c0b5645be19feb946b947ba
Roman Khrebtov <r.hrebtov@busfor.com>
2017-10-02 00:48:39 +0300

    Select for parent category

--- lib/cookery_admin/views/category_view.ex
+++ lib/cookery_admin/views/category_view.ex
@@ -1,3 +1,21 @@
 defmodule CookeryAdmin.CategoryView do
   use CookeryAdmin, :view
+
+  alias Cookery.Data.Recipes
+
+  def categories_for_select do
+    Recipes.categories_tree()
+    |> categories_list()
+  end
+
+  defp categories_list(categories, level \\ 0) do
+    Enum.map(categories,
+      fn({category, child_categories}) ->
+        title = String.duplicate("- ", level) <> category.name
+        child_items = categories_list(child_categories, level + 1)
+        [{title, category.id} | child_items]
+      end
+    )
+    |> Enum.concat()
+  end
 end
