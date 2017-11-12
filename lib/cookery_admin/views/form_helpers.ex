0e1da570224ef56593c452e2240228adb101ecfa
Roman Khrebtov <roman@alltmb.ru>
2017-11-12 16:28:49 +0300

    Translate form fields

--- lib/cookery_admin/views/form_helpers.ex
+++ lib/cookery_admin/views/form_helpers.ex
@@ -6,34 +6,39 @@ defmodule CookeryAdmin.FormHelpers do
   @doc """
   Form helpers for fields with label, input, error message.
   """
-  def form_field(form, changeset, field) do
-    form_field(form, changeset, field, &text_input/3)
+  def form_field(form, changeset, field, title) do
+    form_field(form, changeset, field, title, &text_input/3)
   end
-  def form_field_textarea(form, changeset, field) do
-    form_field(form, changeset, field, &textarea/3)
+  def form_field_textarea(form, changeset, field, title) do
+    form_field(form, changeset, field, title, &textarea/3)
   end
-  def form_field_password(form, changeset, field) do
-    form_field(form, changeset, field, &password_input/3)
+  def form_field_password(form, changeset, field, title) do
+    form_field(form, changeset, field, title, &password_input/3)
   end
-  def form_field_file(form, changeset, field) do
-    form_field(form, changeset, field, &file_input/3)
+  def form_field_file(form, changeset, field, title) do
+    form_field(form, changeset, field, title, &file_input/3)
   end
-  def form_field_select(form, changeset, field, values_list) do
-    form_field(form, changeset, field, &(select(&1, &2, values_list, &3)))
+  def form_field_select(form, changeset, field, values_list, title) do
+    form_field(
+      form, changeset, field, title,
+      &(select(&1, &2, values_list, &3))
+    )
   end
-  def form_field_multiple_select(form, changeset, field, values_list) do
+  def form_field_multiple_select(form, changeset, field, title, values_list) do
     html_options = [style: "height: 300px;"]
-    form_field(form, changeset, field,
-      &(multiple_select(&1, &2, values_list, Keyword.merge(&3, html_options))))
+    form_field(
+      form, changeset, field, title,
+      &(multiple_select(&1, &2, values_list, Keyword.merge(&3, html_options)))
+    )
   end
 
   @doc """
   Generates container tag with label, input, error message.
   """
-  def form_field(form, changeset, field, field_type) do
+  def form_field(form, changeset, field, title, field_type) do
     content_tag :div, class: "form-group #{error_class(changeset, field)}" do
       [
-        label(form, field),
+        label(form, field, title),
         field_type.(form, field, class: "form-control"),
         error_tag(form, field),
       ]
